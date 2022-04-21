015a759580c2ef431007161dff2412411a072dc5
xNewz <pargornruasijan@gmail.com>
2022-04-22 00:49:16 +0700

    add: create method bubble_sort

--- csharp-bubble-sort/Program.cs
+++ csharp-bubble-sort/Program.cs
@@ -4,9 +4,13 @@ namespace csharp_bubble_sort
 {
     class Program
     {
+        static int[] bubble_sort(int[] a, int n)
+        {
+            return a;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
