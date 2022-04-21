9f458b2c51b8b660274434df37afa37c6058e1ac
xNewz <pargornruasijan@gmail.com>
2022-04-22 00:48:09 +0700

    add: create project

---
+++ csharp-bubble-sort/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_bubble_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
