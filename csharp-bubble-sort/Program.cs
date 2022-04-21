3f4db23a63b26d7d89f0f4ff14cbb396763d2363
xNewz <pargornruasijan@gmail.com>
2022-04-22 00:51:05 +0700

    add: create for loop j

--- csharp-bubble-sort/Program.cs
+++ csharp-bubble-sort/Program.cs
@@ -9,7 +9,16 @@ namespace csharp_bubble_sort
             int tmp;
             for (int i = 0; i < n-1; i++)
             {
-                
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        tmp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = tmp;
+                    }
+
+                }
             }
             return a;
         }
